6675e2727178ca1a55c3bf2533ebe021a26d9e83
Ayan-webDEV <ayanalamdev@gmail.com>
2025-11-26 19:08:25 +0530

    rendering format changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Home/Header";
 import Home from "./Components/Home/Home";
+// import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <>
+      <Header />
       <Home />
+      <Footer />
     </>
   );
 };
