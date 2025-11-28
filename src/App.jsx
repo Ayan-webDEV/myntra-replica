d3ce56a425ad4ea6312c182cb187b48c1222bd90
Ayan-webDEV <ayanalamdev@gmail.com>
2025-11-28 19:17:31 +0530

    visibility conditions applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import Footer from "./Components/Footer/Footer";
+import "./index.css";
 import Header from "./Components/Home/Header";
 import Home from "./Components/Home/Home";
-// import Footer from "./components/Footer/Footer";
+import Footer from "./Components/Footer/Footer";
+import NotVisible from "./Components/NotVisible";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Home />
-      <Footer />
+      <div className="desktop-visible">
+        <Header />
+        <Home />
+        <Footer />
+      </div>
+      <div className="not-visible">
+        <NotVisible />
+      </div>
     </>
   );
 };
